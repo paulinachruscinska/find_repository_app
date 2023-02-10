c49f4d074654ec76e3cbc8d2f2c56f68d38a386c
Your Name <you@example.com>
2023-02-10 12:30:40 +0100

    updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,7 @@ import navData from "../utilities/navData";
 export default function Navigation(){
     return(
         <nav>
+            <div className='nav__logo'>.find</div>
             {navData.map(({list, id})=><div className='nav__list' key={id}>{list}</div>)}
         </nav>
     )
