2c60f15cc09d824194090ffb11a63f78409d1bcf
Your Name <you@example.com>
2023-02-11 13:59:58 +0100

    Add some styles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,7 @@
 export default function Main() {
     return (
         <main>
-            <h1>Find</h1>
+            <h1 className='header'>Find</h1>
             <h2>repository</h2>
             <input type='search'/>
         </main>
